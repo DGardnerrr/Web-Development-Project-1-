d7c072f2b457b60e9129c57949b6b2ada8a73c58
Deanna Gardner <dgardner5440@gmail.com>
2020-11-20 15:59:05 -0500

    created header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,13 +8,20 @@
     <router-view /> -->
   </div>
 </template>
-
+<script>
+import Header from "@/views/Header.vue";
+export default {
+  name: "app",
+  components: {
+    Header,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ src/views/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <img alt="logo" src="http://placekitten.com/300/150" />
+    <ol class="nav">
+      <li class="nav-item">Features</li>
+      <li class="nav-item">About</li>
+      <li class="nav-item">Testimonial</li>
+      <li class="nav-item">Download</li>
+      <li class="nav-item">Contact</li>
+    </ol>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+.nav-item {
+  display: inline;
+  padding: 10px 15px;
+}
+</style>
